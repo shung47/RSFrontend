59b90d86cac52f6062ecebd27c2b2f0e3342f27a
SeanLee <seanlee@avnet.com>
2021-08-26 11:55:15 +0800

    ...

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import NevBar from './components/NevBar';
+import Tickets from './components/Tickets';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <NevBar/>
+      <div className ="content">
+        <Switch>
+          <Route exact path="/Login">
+            <Login/>
+          </Route>
+          <Route exact path="/SignUp">
+            <SignUp/>
+          </Route>
+          <Route exact path="/Tickets">
+            <div className ="Ticket-content">
+              <Tickets/>
+            </div>
+          </Route>
+        </Switch>
+      </div>
     </div>
+    </Router>
   );
 }
 
